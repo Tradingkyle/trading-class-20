7d34782f9a9e2e51b7cd86e3a887a3bbcfb88c80
Tradingkyle <134735039+Tradingkyle@users.noreply.github.com>
2026-02-01 12:30:04 -0500

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://player.cloudinary.com/embed/?cloud_name=dp8fbcegv&public_id=Dave_Tems_-_Raindance_audio_bwhuka", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dp8fbcegv/video/upload/v1769966731/Dave_-_Raindance_ft._Tems_-_Santan_Dave_wc1ud2.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
